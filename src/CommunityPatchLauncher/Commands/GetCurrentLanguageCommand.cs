91382309d9ed7de213d448b3bf3d56ff76b2bac9
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-06-28 20:27:29 +0200

    Adding command for application restart
    Adding command for getting current language

---
+++ src/CommunityPatchLauncher/Commands/GetCurrentLanguageCommand.cs
@@ -0,0 +1,36 @@
+﻿using CommunityPatchLauncherFramework.Settings.Container;
+using CommunityPatchLauncherFramework.Settings.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityPatchLauncher.Commands
+{
+    /// <summary>
+    /// This command will return you the current language of the application
+    /// </summary>
+    internal class GetCurrentLanguageCommand : BaseDataCommand
+    {
+        /// <inheritdoc/>
+        public override bool CanExecute(object parameter)
+        {
+            return parameter is SettingManager;
+        }
+
+        /// <inheritdoc/>
+        public override void Execute(object parameter)
+        {
+            if (parameter is SettingManager manager)
+            {
+                SettingPair language = manager.GetValue("language");
+                if (language != null)
+                {
+                    data = language.GetValue<string>();
+                }
+            }
+            ExecutionDone();
+        }
+    }
+}
---
+++ src/CommunityPatchLauncher/Commands/RestartApplicationCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CommunityPatchLauncher.Commands
+{
+    /// <summary>
+    /// This command will restart the application
+    /// </summary>
+    internal class RestartApplicationCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        /// <inheritdoc/>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public void Execute(object parameter)
+        {
+            Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+        }
+    }
+}
