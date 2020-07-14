5f522c04a4f39d5e9678f07bd3c016a0050a31ac
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-07-14 19:24:41 +0200

    Adding reader for ini files

---
+++ src/CommunityPatchLauncher/Settings/Reader/IniStringReader.cs
@@ -0,0 +1,41 @@
+﻿using CommunityPatchLauncherFramework.Settings.Container;
+using CommunityPatchLauncherFramework.Settings.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CommunityPatchLauncher.Settings.Reader
+{
+    class IniStringReader : ISettingReader
+    {
+        private const string SCOPE_REGEX = "\\[[a-zA-Z0-9]+\\]";
+
+        public HashSet<SettingPair> LoadSettings(string connectionString)
+        {
+            HashSet<SettingPair> returnSettings = new HashSet<SettingPair>();
+            string[] lines = connectionString.Split(new[] { "\n" }, StringSplitOptions.None);
+            string currentScope = "";
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                if (Regex.IsMatch(line, SCOPE_REGEX))
+                {
+                    currentScope = line.Replace("[", "").Replace("]", "").Trim();
+                    continue;
+                }
+
+                if (!line.Contains("="))
+                {
+                    continue;
+                }
+
+                string[] data = line.Split('=');
+                returnSettings.Add(new SettingPair(currentScope + "/" + data[0], data[1]));
+            }
+            return returnSettings;
+        }
+    }
+}
