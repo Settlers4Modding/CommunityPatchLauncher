4480d841ccaac2421a9f3627a5f93d0208ac5ffa
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-01 09:46:07 +0200

    Adding methods to get the installation folder
    Adding method to manualy select the folder
    Adding method to get the folder from the registry

---
+++ src/CommunityPatchLauncher/Commands/InstallationFromRegistryCommand.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System.IO;
+
+namespace CommunityPatchLauncher.Commands
+{
+    /// <summary>
+    /// This class will find the installation folder from the registry
+    /// </summary>
+    internal class InstallationFromRegistryCommand : BaseDataCommand
+    {
+        /// <summary>
+        /// Path of the registry key to read
+        /// </summary>
+        private const string REGISTER_KEY = @"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Ubisoft\Launcher\Installs\11785";
+
+        /// <summary>
+        /// Name of the registry key to read
+        /// </summary>
+        private const string KEY_NAME = "InstallDir";
+
+        /// <inheritdoc/>
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public override void Execute(object parameter)
+        {
+            string installPath = Registry.GetValue(REGISTER_KEY, KEY_NAME, "").ToString();
+            data = File.Exists(installPath + "S4_Main.exe") ? installPath : "";
+            ExecutionDone();
+        }
+    }
+}
