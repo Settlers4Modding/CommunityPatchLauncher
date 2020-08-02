99c1ce074784d5b4681947c838d9678dff2792eb
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-02 10:23:27 +0200

    Adding new task to unzip the community patch

--- src/CommunityPatchLauncher/Tasks/UnzipCommunityPatchTask.cs
+++ src/CommunityPatchLauncher/Tasks/UnzipCommunityPatchTask.cs
@@ -2,63 +2,145 @@
 using CommunityPatchLauncherFramework.Settings.Manager;
 using CommunityPatchLauncherFramework.TaskPipeline.EventData;
 using CommunityPatchLauncherFramework.TaskPipeline.Tasks;
-using SevenZip.Compression.LZMA;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommunityPatchLauncher.Tasks
 {
+    /// <summary>
+    /// This class will unzip the community patch
+    /// </summary>
     public class UnzipCommunityPatchTask : IProgressTask
     {
-        private readonly int workload;
+        /// <summary>
+        /// The total workload which should be done
+        /// </summary>
+        private readonly int totalWorkload;
+
+        /// <summary>
+        /// The settings manager to use
+        /// </summary>
         private SettingManager settingsManager;
 
-        public HashSet<SettingPair> Settings => settings;
-        private HashSet<SettingPair> settings;
+        /// <summary>
+        /// The internal settings of this task
+        /// </summary>
+        public HashSet<SettingPair> Settings { get; private set; }
 
-        public bool AbortOnError => throw new NotImplementedException();
+        /// <summary>
+        /// Should this class abort on error
+        /// </summary>
+        public bool AbortOnError { get; private set; }
 
+        /// <summary>
+        /// Did the progress change
+        /// </summary>
         public event EventHandler<TaskProgressChanged> ProgressChanged;
+
+        /// <summary>
+        /// Is the task already completed
+        /// </summary>
         public event EventHandler<TaskDone> TaskComplete;
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
         public UnzipCommunityPatchTask()
         {
-            workload = 2;
+            AbortOnError = true;
+            totalWorkload = 100;
         }
 
+        /// <inheritdoc>
         public void Init(SettingManager settingManager, HashSet<SettingPair> taskSettings)
         {
             this.settingsManager = settingManager;
-            this.settings = taskSettings;
+            Settings = taskSettings;
         }
 
+        /// <inheritdoc>
         public bool Execute(bool previousTaskState)
         {
             SettingPair upToDate = Settings.Where((obj) => obj.Key == "UpToDate").First();
             if (upToDate == null)
             {
+                TaskDone();
                 return false;
             }
             if (upToDate.GetValue<bool>())
             {
+                TaskDone();
                 return true;
             }
-            SevenZip.Compression.LZMA.Encoder encoder = new SevenZip.Compression.LZMA.Encoder();
             SettingPair filePath = Settings.Where((obj) => obj.Key == "PatchInstaller").First();
+
             if (filePath == null)
             {
+                TaskDone();
                 return true;
             }
+            string zipFilePath = filePath.GetValue<string>();
+            if (!File.Exists(zipFilePath))
+            {
+                TaskDone();
+                return false;
+            }
+            string unzipFolder = Path.GetTempPath() + Guid.NewGuid() + "\\";
+            
+            using (ZipArchive archive = new ZipArchive(new FileStream(zipFilePath, FileMode.Open), ZipArchiveMode.Read))
+            {
+                int filesToUnzip = archive.Entries.Count;
+                int currentFile = 0;
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    currentFile++;
+                    int currentProgress = (int)((currentFile / (float)filesToUnzip) * 100);
+                    EventHandler<TaskProgressChanged> handler = ProgressChanged;
+                    FileInfo info = new FileInfo(unzipFolder + entry.FullName);
+                    if (!Directory.Exists(info.DirectoryName))
+                    {
+                        Directory.CreateDirectory(info.DirectoryName);
+                    }
+                    try
+                    {
+                        if (info.DirectoryName + "\\" == info.FullName)
+                        {
+                            handler?.Invoke(this, new TaskProgressChanged(totalWorkload, currentProgress));
+                            continue;
+                        }
+                        entry.ExtractToFile(info.FullName, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        TaskDone();
+                        return false;
+                    }
+
+                    handler?.Invoke(this, new TaskProgressChanged(totalWorkload, currentProgress));
+                }
+            }
+
+            Settings.Add(new SettingPair("extractPath", unzipFolder));
+            TaskDone();
             return true;
         }
 
+        /// <summary>
+        /// The task is completed
+        /// </summary>
+        private void TaskDone()
+        {
+            EventHandler<TaskDone> handler = TaskComplete;
+            handler?.Invoke(this, new TaskDone(totalWorkload));
+        }
+
+        /// <inheritdoc>
         public int GetStepCount()
         {
-            return workload;
+            return totalWorkload;
         }
     }
 }
