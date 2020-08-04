69d3c400316a0f5a2547b1619f939d276ffd825d
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-04 10:40:23 +0200

    Adding new task to offline download all the files
    Extending default download task for better inhertion
    Fixed bug with remote version

---
+++ src/CommunityPatchLauncher/Tasks/Factories/DownloadOfflineFilesFactory.cs
@@ -0,0 +1,28 @@
+﻿using CommunityPatchLauncherFramework.TaskPipeline.Factory;
+using CommunityPatchLauncherFramework.TaskPipeline.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityPatchLauncher.Tasks.Factories
+{
+    internal class DownloadOfflineFilesFactory : ITaskFactory
+    {
+        private readonly string versionInformationUrl;
+
+        public DownloadOfflineFilesFactory(string versionInformationUrl)
+        {
+            this.versionInformationUrl = versionInformationUrl;
+        }
+
+        public List<ITask> GetTasks()
+        {
+            List<ITask> returnList = new List<ITask>();
+            returnList.Add(new DownloadVersionInformation(versionInformationUrl));
+            returnList.Add(new DownloadAllPatches());
+            return returnList;
+        }
+    }
+}
