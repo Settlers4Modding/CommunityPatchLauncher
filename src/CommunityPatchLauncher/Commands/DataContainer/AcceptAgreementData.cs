864f88d7b4bc8a0800677c10505ece4ebfedd90f
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-10 10:24:52 +0200

    Adding summary blocks

--- src/CommunityPatchLauncher/Commands/DataContainer/AcceptAgreementData.cs
+++ src/CommunityPatchLauncher/Commands/DataContainer/AcceptAgreementData.cs
@@ -2,13 +2,38 @@
 
 namespace CommunityPatchLauncher.Commands.DataContainer
 {
+    /// <summary>
+    /// This class is a data container for the accept agreement command
+    /// </summary>
     internal class AcceptAgreementData
     {
+        /// <summary>
+        /// Was the agreement accepted
+        /// </summary>
         public bool Agreement { get; }
+
+        /// <summary>
+        /// Is the game folder set and okay
+        /// </summary>
         public bool FolderSet { get; }
+
+        /// <summary>
+        /// The game folder to use
+        /// </summary>
         public string GameFolder { get; }
+
+        /// <summary>
+        /// The language to use
+        /// </summary>
         public LanguageItem Language { get; }
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="agreement">Was the agreement accepted</param>
+        /// <param name="folderSet">Is the game folder which was set okay</param>
+        /// <param name="gameFolder">The path to the game folder</param>
+        /// <param name="language">The language to use</param>
         public AcceptAgreementData(bool agreement, bool folderSet, string gameFolder, LanguageItem language)
         {
             Agreement = agreement;
