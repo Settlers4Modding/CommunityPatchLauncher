15d1f739a1ee486fe30ee3371ff6c8f5a91753ae
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-10 10:16:07 +0200

    Adding more summary blocks

--- src/CommunityPatchLauncher/BindingData/Container/LanguageItem.cs
+++ src/CommunityPatchLauncher/BindingData/Container/LanguageItem.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CommunityPatchLauncher.BindingData.Container
 {
+    /// <summary>
+    /// A new language item
+    /// </summary>
     public class LanguageItem
     {
+        /// <summary>
+        /// The display name of the language
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// The iso code of the language
+        /// </summary>
         public string IsoCode { get; }
 
+        /// <summary>
+        /// Create a new language element
+        /// </summary>
+        /// <param name="name">The display name of the language</param>
+        /// <param name="isoCode">The iso code of the language</param>
         public LanguageItem(string name, string isoCode)
         {
             Name = name;
--- src/CommunityPatchLauncher/BindingData/Container/Patch.cs
+++ src/CommunityPatchLauncher/BindingData/Container/Patch.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 
 namespace CommunityPatchLauncher.BindingData.Container
 {
+    /// <summary>
+    /// A new patch you can use for downloading
+    /// </summary>
     public class Patch
     {
+        /// <summary>
+        /// The display name of the patch
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// The enum value for the patch
+        /// </summary>
         public AvailablePatches RealPatch { get; }
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="realPatch">The real patch to use</param>
         public Patch(AvailablePatches realPatch)
         {
             
