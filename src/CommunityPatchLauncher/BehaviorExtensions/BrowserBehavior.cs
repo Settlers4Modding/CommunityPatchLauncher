a570de2dc0259303edece17113dcd8a81dfd0929
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-10 10:13:52 +0200

    Adding missing summary blocks

--- src/CommunityPatchLauncher/BehaviorExtensions/BrowserBehavior.cs
+++ src/CommunityPatchLauncher/BehaviorExtensions/BrowserBehavior.cs
@@ -1,29 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace CommunityPatchLauncher.BehaviorExtensions
 {
+    /// <summary>
+    /// This class adds a new behavior to the webbrowser
+    /// </summary>
     public class BrowserBehavior
     {
+        /// <summary>
+        /// New html property
+        /// </summary>
         public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
             "Html", typeof(string), typeof(BrowserBehavior), new FrameworkPropertyMetadata(OnHtmlChanged));
 
+        /// <summary>
+        /// Get the content of the html property
+        /// </summary>
+        /// <param name="browser">The browser to use</param>
+        /// <returns>The content as string</returns>
         [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
         public static string GetHtml(WebBrowser browser)
         {
             return (string)browser.GetValue(HtmlProperty);
         }
 
+        /// <summary>
+        /// Set the content of the html property
+        /// </summary>
+        /// <param name="browser">The browser to use</param>
+        /// <param name="value">The value to set</param>
         public static void SetHtml(WebBrowser browser, string value)
         {
             browser.SetValue(HtmlProperty, value);
         }
 
+        /// <summary>
+        /// If the html attribute did change
+        /// </summary>
+        /// <param name="dependencyObject">The depending pbject</param>
+        /// <param name="e">Event arguments</param>
         static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
         {
             var browser = dependencyObject as WebBrowser;
