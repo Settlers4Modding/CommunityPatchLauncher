9d918eb4f43ce128403ea11fddd6f26de3e044a0
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-12 12:46:06 +0200

    Adding border to main menu
    Dynamic window image

--- src/CommunityPatchLauncher/BindingData/Converter/BoolToVisibilityConverter.cs
+++ src/CommunityPatchLauncher/BindingData/Converter/BoolToVisibilityConverter.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace CommunityPatchLauncher.BindingData.Converter
@@ -12,7 +13,11 @@ namespace CommunityPatchLauncher.BindingData.Converter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Hidden;
+            }
+            return Visibility.Hidden;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
