825463dda68808e7e5c099283a22d92bdeed47b7
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-12 12:57:54 +0200

    Adding summary blocks

--- src/CommunityPatchLauncher/BindingData/Converter/BoolToVisibilityConverter.cs
+++ src/CommunityPatchLauncher/BindingData/Converter/BoolToVisibilityConverter.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
 namespace CommunityPatchLauncher.BindingData.Converter
 {
+    /// <summary>
+    /// This class will convert a bool into a visiblity
+    /// </summary>
     internal class BoolToVisibilityConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool boolValue)
@@ -20,9 +20,10 @@ namespace CommunityPatchLauncher.BindingData.Converter
             return Visibility.Hidden;
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return null;
         }
     }
 }
