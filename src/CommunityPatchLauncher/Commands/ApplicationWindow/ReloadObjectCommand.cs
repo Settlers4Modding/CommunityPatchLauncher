2c23c8c0b68a32de30104a2889abc14342d67620
XanatosX <10531466+XanatosX@users.noreply.github.com>
2020-08-13 17:45:40 +0200

    Fixed bug and adding summary blocks

--- src/CommunityPatchLauncher/Commands/ApplicationWindow/ReloadObjectCommand.cs
+++ src/CommunityPatchLauncher/Commands/ApplicationWindow/ReloadObjectCommand.cs
@@ -2,22 +2,38 @@
 
 namespace CommunityPatchLauncher.Commands.ApplicationWindow
 {
+    /// <summary>
+    /// Reload a given object
+    /// </summary>
     internal class ReloadObjectCommand : BaseCommand
     {
+        /// <summary>
+        /// The object to reload
+        /// </summary>
         private readonly IViewModelReloadable reloadable;
 
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="reloadable">The object to reload</param>
         public ReloadObjectCommand(IViewModelReloadable reloadable)
         {
             this.reloadable = reloadable;
         }
 
+        /// <inheritdoc/>
         public override bool CanExecute(object parameter)
         {
             return reloadable != null;
         }
 
+        /// <inheritdoc/>
         public override void Execute(object parameter)
         {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
             reloadable.Reload();
         }
     }
