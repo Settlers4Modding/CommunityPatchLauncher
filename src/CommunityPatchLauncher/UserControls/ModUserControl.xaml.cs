d9505de17b114f06e34faff3b25bf0abafa7c4c8
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2020-10-27 17:56:00 +0100

    Adding summary blocks
    Removed unused includes

--- src/CommunityPatchLauncher/UserControls/ModUserControl.xaml.cs
+++ src/CommunityPatchLauncher/UserControls/ModUserControl.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using CommunityPatchLauncher.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CommunityPatchLauncher.UserControls
 {
@@ -21,6 +9,10 @@ namespace CommunityPatchLauncher.UserControls
     /// </summary>
     public partial class ModUserControl : UserControl
     {
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="parentWindow"></param>
         public ModUserControl(Window parentWindow)
         {
             InitializeComponent();
--- src/CommunityPatchLauncher/ViewModels/ModModelView.cs
+++ src/CommunityPatchLauncher/ViewModels/ModModelView.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace CommunityPatchLauncher.ViewModels
 {
+    /// <summary>
+    /// This class is the view model for the mods view
+    /// </summary>
     internal class ModModelView : BaseViewModel
     {
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="parentWindow"></param>
         public ModModelView(Window parentWindow) : base(parentWindow)
         {
 
