e4a75fc037e3390f2b362c502de4a5a19315402d
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2021-01-30 20:11:52 +0100

    Add history plus

--- src/CommunityPatchLauncher/Enums/AvailablePatches.cs
+++ src/CommunityPatchLauncher/Enums/AvailablePatches.cs
@@ -6,7 +6,7 @@
     public enum AvailablePatches
     {
         HistoryEdition,
-        CommunityPatchLight,
+        HistoryEditionPlus,
         CommunityPatch,
         CommunityPatchPTR,
         CommunityPatchDLC
