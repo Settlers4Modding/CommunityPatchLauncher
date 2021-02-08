065ec69baf992738cb9779516a2a1b8f167773a4
XanatosX <10531466+XanatosX@users.noreply.github.com>
2021-02-08 19:12:06 +0100

    Add fix for single screen systems

--- src/CommunityPatchLauncher/ViewModels/BaseViewModel.cs
+++ src/CommunityPatchLauncher/ViewModels/BaseViewModel.cs
@@ -158,6 +158,8 @@ namespace CommunityPatchLauncher.ViewModels
             currentWindow = window;
             if (currentWindow != null)
             {
+                currentWindow.MaxHeight = SystemParameters.WorkArea.Height;
+                currentWindow.MaxWidth = SystemParameters.WorkArea.Width;
                 int currentWidth = settingManager.GetValue<int>("Width");
                 int currentHeight = settingManager.GetValue<int>("Height");
                 if (currentWidth != 0 && currentHeight != 0)
