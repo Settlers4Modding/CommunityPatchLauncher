18915d1f9c81a9e7f353a7af4e3bf5b5ddc5534e
Ausgeben <36706695+Ausgeben@users.noreply.github.com>
2021-02-26 15:11:48 +0100

    Class made

---
+++ src/CommunityPatchLauncher/Commands/ApplicationWindow/ResetGameCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityPatchLauncher.Commands.ApplicationWindow
+{
+    /// <summary>
+    /// Yes/No Dialog: Reset Save Games
+    /// </summary>
+    class ResetGameCommand : BaseDataCommand
+    {
+        /// <inheritdoc/>
+        public override bool CanExecute(object parameter)
+        {
+            return;
+        }
+
+        /// <inheritdoc/>
+        public override void Execute(object parameter)
+        {
+
+
+        }
+}
