e97c350c71c8572cc7a51f403ec303be256c9048
Leonard <gaming-leonard@web.de>
2021-10-30 16:59:25 +0200

    a
    a

--- src/CommunityPatchLauncher/Windows/MainWindow.xaml.cs
+++ src/CommunityPatchLauncher/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using CommunityPatchLauncher.ViewModels;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
 using System.Windows;
 
 namespace CommunityPatchLauncher.Windows
@@ -16,5 +20,61 @@ namespace CommunityPatchLauncher.Windows
             InitializeComponent();
             this.DataContext = new MainWindowModel(this);
         }
+
+
+
+        #region Downloader
+        private void UpgradetoSettlersUnited(object sender, RoutedEventArgs e)
+        {
+            UpgradetoSettlersUnited();
+
+        }
+        private void UpgradetoSettlersUnited()
+        {
+           
+            string URI = "https://files.settlers-united.com/Settlers-United.exe";
+            DownloadFileAsync(URI, "SettlersUnitedSetup.exe", "Upgrade auf Settlers United (Beta)" , true);
+        }
+        private void DownloadFileAsync(string URI, string File, string Name, bool SettlersUnited = false)
+        {
+            DownlaodPanel.Visibility = Visibility.Visible;
+            DownlaodLabel.Content = "Herunterladen des Updates..." + "\n" + Name;
+
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadProgressChanged += DownloadProgressChanged;
+                    wc.DownloadFileCompleted += DownloadFileEventCompletedUnited;
+                    wc.DownloadFileAsync(new Uri(URI), File);
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+           
+        }
+
+
+        private void DownloadFileEventCompletedUnited(object sender, AsyncCompletedEventArgs e)
+        {
+            //ToDo Install first HE!!! @Leonards05 | Pumpline#5578
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "SettlersUnitedSetup.exe",
+
+                Verb = "runas"
+            };
+            Process.Start(startInfo);
+            Environment.Exit(0);
+        }
+
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            DownlaodPanel.Visibility = Visibility.Visible;
+            ProgressBar.Value = e.ProgressPercentage;
+        }
+        #endregion
     }
 }
